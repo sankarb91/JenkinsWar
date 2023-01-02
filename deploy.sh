b657fe33bc307ae0b17a1789d1c674978de38403
saravananajay <34689245+saravananajay@users.noreply.github.com>
2023-01-02 22:48:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@
 
 packagename=$1
 
+echo "$packagename"
 echo "---deleting olss package code---"
 rm -rf /home/tamilnadu/*
 echo "---Downloading package---"
-curl --user admin:Nexus123 -o mylatest.war $packagename
+curl --user admin:Nexus123 -o mylatest.war "$packagename"
 unzip mylatest.war -d /home/tamilnadu/
 rsync -avz /home/tamilnadu/* tamilnadu@44.208.33.213:/usr/local/tomcat9/webapps/jan03-demo/
